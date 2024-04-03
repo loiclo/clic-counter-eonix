01ffb7413d890fc380d9df08f0c9e01e98b6f62d
loiclo <lecinloic07@gmail.com>
2024-04-03 10:39:03 +0200

    ✨ Home > add counter funcion on a button

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Home({
-  onIncrement,
-  counter,
-}) {
+function Home (){
+  const [counter, setCounter] = useState(0); // Initialise le compteur à 0
+
+  const onIncrement = () => {
+    setCounter(counter + 1); // Incrémente le compteur 
+  };
+
   return (
     <div>
-      <button>
+      <button onClick={onIncrement}>
         Vous avez cliqué {counter} fois
       </button>
     </div>
-  );
-};
+  )
+}
 
 export default Home;
