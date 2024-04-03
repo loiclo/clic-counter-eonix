700ab0848e8b9f07d2b1aaff6355aa75f829c7aa
loiclo <lecinloic07@gmail.com>
2024-04-03 11:59:17 +0200

    ✨ Home > added decrementation

--- src/views/Home.js
+++ src/views/Home.js
@@ -7,10 +7,20 @@ function Home (){
     setCounter(counter + 1); // Incrémente le compteur 
   };
 
+  const onDecrement = () => {
+    if (counter > 0) { // Vérifie si le compteur est supérieur à 0 avant de décrémenter
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <div>
       <button onClick={onIncrement}>
         Vous avez cliqué {counter} fois
+      </button>
+
+      <button onClick={onDecrement} disabled={counter === 0}>
+       Annuler votre clic {counter}
       </button>
     </div>
   )
