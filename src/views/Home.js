73c846bb7ce05170057bb7889f6d31844582c7ee
loiclo <lecinloic07@gmail.com>
2024-04-09 16:28:00 +0200

    ♻️ Home > Counter > create counter component

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,32 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
+import Counter from '../components/Counter';
 
-function Home (){
-  const [counter, setCounter] = useState(0); // Initialise le compteur à 0
-
-  const onIncrement = () => {
-    setCounter(counter + 1); // Incrémente le compteur 
-  };
-
-  const onDecrement = () => {
-    if (counter > 0) { // Vérifie si le compteur est supérieur à 0 avant de décrémenter
-      setCounter(counter - 1);
-    }
-  };
-
+function Home() {
   return (
     <div>
-    Vous avez cliqué {counter} fois
-      <div>
-        <button onClick={onIncrement}>
-          Cliquer ici
-        </button>
-
-        <button onClick={onDecrement} disabled={counter === 0}> 
-        Annuler votre clic
-        </button>
-      </div>
+      <Counter />
     </div>
-  )
+  );
 }
-
 export default Home;
